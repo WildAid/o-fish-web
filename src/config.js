c3da9218a9da39ee324015492419c2b028c0218c
Sheeri <sheeri.cabral@mongodb.com>
2020-09-28 11:12:12 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,27 +1,27 @@
 const config = {
   appName: 'WildAid O-FISH',
   realmServiceName: 'mongodb-atlas',
-  realmAppId: 'o-fish-trial-dmssx',
+  realmAppId: '',
   database: 'wildaid',
   chartsConfig: {
-    baseUrl: "https://charts.mongodb.com/charts-o-fish-psrir",
+    baseUrl: "",
     "boardings": {
-      chartId: "77702f8f-71f2-4650-a05b-ea0126943bf8"
+      chartId: ""
     },
     "boarding-compliance":{
-      chartId: "26107bcf-68b4-4d9e-8bd1-ee0fb191a230"
+      chartId: ""
     },
     "patrol-hours":{
-      chartId: "caedf1d9-d7fd-4bb4-b6b2-30e3b63757bc"
+      chartId: ""
     },
     "compliance-rate":{
-      chartId: "d36c9cd0-b345-4997-bccc-ca0ded7d6f3f"
+      chartId: "
     },
     "boardings-count-chart":{
-      chartId: "f7353834-d9f1-41de-9884-c7ac9e7f01ee"
+      chartId: ""
     },
     "citations-and-warnings":{
-      chartId: "12f86c49-0ca4-4994-8dbc-5ceac48ded82"
+      chartId: ""
     }
   }
 };
