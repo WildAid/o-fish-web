437bfcbc119aa476960a87e24650713f148af3d4
Dusch4593 <brandondusch@gmail.com>
2020-10-20 07:30:08 -0400

    changed span attribute from 'class' to 'className' to fix issue #331

--- src/components/partials/filter-panel/filter-panel.component.js
+++ src/components/partials/filter-panel/filter-panel.component.js
@@ -150,7 +150,6 @@ class FilterPanel extends Component {
     const { isFilterPanelShown, filterParts } = this.state;
     const { options, configuration, t } = this.props;
     const filterPartNames = filterParts.map((item) => item.name);
-
     return (
       <div className="flex-row align-center">
         <div className="flex-row">
@@ -170,7 +169,7 @@ class FilterPanel extends Component {
         </div>
         <div className="relative">
         <div className="filter-btn blue-btn icon-radius d-flex flex-row align-end" onClick={this.showFilter}>
-            <span class="material-icons icon-font">filter_alt</span> { filterParts.length ? `(${filterParts.length})` : "" }
+            <span className="material-icons icon-font">filter_alt</span> { filterParts.length ? `(${filterParts.length})` : "" }
           </div>
           <div
             className={
