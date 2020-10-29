72e0ec4ba69d62b2fb192c6f483dd4a38f033601
Sheeri Cabral <awfief@gmail.com>
2020-10-29 14:20:53 -0400

    taking out inline CSS in sections not used and in violations which is now used

--- src/components/boardings/boarding-view/notes/notes.section.js
+++ src/components/boardings/boarding-view/notes/notes.section.js
@@ -9,7 +9,7 @@ export default class NotesSection extends Component {
       <section>
           <h3>Note {itemNo++}</h3>
           <div className="row">
-            <TextField label="Note:" style={{width: "100%"}} nname="note" value={item.note} className="half-row" onChange={e => this.setFieldValue("note", e.target.value)}/>
+            <TextField label="Note:" nname="note" value={item.note} className="half-row" onChange={e => this.setFieldValue("note", e.target.value)}/>
           </div>
       </section>
     )))
@@ -19,8 +19,8 @@ export default class NotesSection extends Component {
     const values = this.props.dataObject.notes;
     return (
       <div class='notes-section section'>
-        <div className="row left-aligned" style={{width:"60%", margin: 0, padding: "10px 2em"}}>
-          <h2 style={{marginTop: 0}}>Notes</h2>
+        <div className="row left-aligned" >
+          <h2>Notes</h2>
         </div>
         {this.getItems(values)}
       </div>
