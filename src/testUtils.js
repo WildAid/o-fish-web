02b5434df0039a7a809351717cf2fd8527c002ed
Lenny LD <lenmorld@gmail.com>
2020-11-29 19:00:13 -0500

    jest and react-testing-library setup for CRA

---
+++ src/testUtils.js
@@ -0,0 +1,26 @@
+// read TESTS.md for setup details
+
+import React from 'react'
+import { BrowserRouter } from "react-router-dom";
+import { render } from '@testing-library/react'
+import { I18nextProvider } from "react-i18next";
+import i18next from "./helpers/i18n/index";
+
+const AllTheProviders = ({ children }) => {
+  return (
+  <I18nextProvider i18n={i18next}>
+      <BrowserRouter>
+        {children}
+      </BrowserRouter>
+  </I18nextProvider>
+  )
+}
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options })
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { customRender as render }
