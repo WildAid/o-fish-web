84518f0d3cc0d9462a9e88939e7691c31c7bebba
Mahabbat Huseynov <mahabbatmh@outlook.com>
2022-08-18 12:05:25 +0400

    Global Administrator - Should be able to see user reports for any user

--- src/components/users/activities/activities.component.js
+++ src/components/users/activities/activities.component.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import withRouter from "../../../helpers/withRouter";
+import UserService from '../../../services/user.service';
+import AuthService from "../../../services/auth.service";
+import FieldDashboardComponent from "../../dashboards/field-dashboard.component";
+
+const userService = UserService.getInstance();
+const authService = AuthService.getInstance();
 
 class UsersActivities extends Component {
+  state = {
+    user: null,
+  }
+
+  componentDidMount() {
+    userService.getUserById(this.props.router.params.id).then(res => {
+      this.setState({ user: res });
+    });
+  }
   render() {
+    const { user } = this.state;
     return (
-      <div className="activities-page">
-        Activities page
+      <div className="flex-column full-view align-center home">
+        {user && <FieldDashboardComponent user={user} />}
       </div>
+      
     );
   }
 }
 
-export default UsersActivities;
+export default withRouter(UsersActivities);
